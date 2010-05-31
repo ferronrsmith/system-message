cdf711491205992cac1f8215c8854201bc8a8187
ferronrsmith <ferronrsmith@34fab576-0c0e-8ea8-2192-8d1a7f0c343c>
2010-05-31 21:36:50 +0000

    changed files

--- branches/mod_sys_message.php
+++ branches/mod_sys_message.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version 0.3
+* @version 0.4.2
 * @package System Message
 * @copyright (C) 2010 Ferron Smith
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
@@ -8,24 +8,17 @@
  
 /** ensure direct access is not attempted */
 defined( '_JEXEC' ) or die( 'Direct Access to this location is not allowed.' );
- 
 ?>
 
-<?php
-	
-	$type = $params->get('message_type');
-	$msg = $params->get('user_message');
+<?php	
+	//Add stylesheet link to the page
+	$doc = &JFactory::getDocument();
+	$doc->addStyleSheet(JURI::root(true) ."/modules/mod_sys_message/settings/settings.css");
 
-	//checks the message type and echo the appropriate sys_message
-	switch($type) {
-		case "alert":
-			echo "<div class=\"alert\">" . $msg . "</div>";
-			break;
-		case "error":
-			echo "<div class=\"error\">" . $msg . "</div>";
-			break;
-		case "message":
-			echo "<div class=\"message\">" . $msg . "</div>";
-			break;
-	}
+	//get user selected values
+	$type = $params->get('message_type');
+	$message = $params->get('user_message');
+
+	//create system message
+	echo "<div class=\"$type\">$message</div>";
 ?>
